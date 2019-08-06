933568bc88b47590421ca6853d9850490994e5e4
ouyangbro <ouyangjianguo@xiaobunet.com>
2019-08-06 10:31:35 +0800

    node数据结构优化，增加T来表示支持任意类型。

---
+++ src/main/java/Node.java
@@ -0,0 +1,28 @@
+/**
+ * Created by ouyang on 2019/8/1.
+ */
+
+public class Node<T>{
+    private T data;
+    private Node next;
+
+    public Node(T data){
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
